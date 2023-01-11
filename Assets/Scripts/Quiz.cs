f75f27c8d5d45064590a79872460bc962c6ab8b5
Andrew Huntington <andrew.huntington@gmail.com>
2023-01-11 19:53:44 +0900

    Finish game

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -14,7 +14,7 @@ public class Quiz : MonoBehaviour
   [Header("Answers")]
   [SerializeField] GameObject[] answerButtons;
   int correctAnswerIndex;
-  bool hasAnsweredEarly;
+  bool hasAnsweredEarly = true;
 
   [Header("Button Colors")]
   [SerializeField] Sprite defaultAnswerSprite;
@@ -24,9 +24,21 @@ public class Quiz : MonoBehaviour
   [SerializeField] Image timerImage;
   Timer timer;
 
-  void Start()
+  [Header("Scoring")]
+  [SerializeField] TextMeshProUGUI scoreText;
+  ScoreKeeper scoreKeeper;
+
+  [Header("ProgressBar")]
+  [SerializeField] Slider progressBar;
+
+  public bool isComplete;
+
+  void Awake()
   {
     timer = FindObjectOfType<Timer>();
+    scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    progressBar.maxValue = questions.Count;
+    progressBar.value = 0;
   }
 
   void Update()
@@ -35,6 +47,12 @@ public class Quiz : MonoBehaviour
       timerImage.fillAmount = timer.fillFraction;
       if (timer.loadNextQuestion)
       {
+        if (progressBar.value == progressBar.maxValue)
+        {
+          isComplete = true;
+          return;
+        }
+
         hasAnsweredEarly = false;
         GetNextQuestion();
         timer.loadNextQuestion = false;
@@ -53,6 +71,7 @@ public class Quiz : MonoBehaviour
     DisplayAnswer(index);
     SetButtonState(false);
     timer.CancelTimer();
+    scoreText.text = $"Score: {scoreKeeper.CalculateScore()}%";
   }
 
   void DisplayAnswer(int index)
@@ -65,6 +84,7 @@ public class Quiz : MonoBehaviour
       questionText.text = "Correct!";
       buttonImage = answerButtons[index].GetComponent<Image>();
       buttonImage.sprite = correctAnswerSprite;
+      scoreKeeper.IncrementCorrectAnswers();
     }
     else
     {
@@ -83,6 +103,8 @@ public class Quiz : MonoBehaviour
       SetDefaultButtonSprites();
       GetRandomQuestion();
       DisplayQuestion();
+      progressBar.value++;
+      scoreKeeper.IncrementQuestionsSeen();
     }
   }
 
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -55,6 +55,5 @@ public class Timer : MonoBehaviour
       }
     }
 
-    Debug.Log(isAnsweringQuestion + ": " + timerValue + " = " + fillFraction);
   }
 }
